be670463787af525d4dc42dbda4c912d7547f3ef
FarisOmerbasic <farisomerbasic@hotmail.com>
2025-02-25 13:45:03 +0100

    Added AlarmTemplate class

--- SmartFactoryBackend/Sensors/EnergyMeterSensor.cs
+++ SmartFactoryBackend/Sensors/EnergyMeterSensor.cs
@@ -3,46 +3,22 @@
     public class EnergyMeterSensor : Sensor
     {
         public double EnergyUsage { get; set; }
-        private double _upperThreshold = 5000;  
-        private double _lowerThreshold = 100;   
 
-      
-        public string EnergyUsageAlert { get; private set; }
-
-        public EnergyMeterSensor(string sensorId) : base(sensorId, "Energy Meter")
+        
+        public EnergyMeterSensor(string sensorId, AlarmTemplate alarmTemplate) : base(sensorId, "Energy Meter", alarmTemplate)
         {
-            EnergyUsage = new Random().Next(100, 1000); 
-            EnergyUsageAlert = CheckEnergyUsageAlert();  
+            EnergyUsage = new Random().Next(100, 1000);
         }
 
-    
+      
         public double GetSensorValue()
         {
             return EnergyUsage;
         }
 
 
-        public void UpdateEnergyUsage()
+        public override string GetAlertLevel()
         {
-            EnergyUsage = new Random().Next(100, 5000);  
-            EnergyUsageAlert = CheckEnergyUsageAlert();  
-        }
-
-
-        private string CheckEnergyUsageAlert()
-        {
-            if (EnergyUsage > _upperThreshold)
-            {
-                return "Alert: High Energy Consumption!";
-            }
-            else if (EnergyUsage < _lowerThreshold)
-            {
-                return "Alert: Low Energy Consumption!";
-            }
-            else
-            {
-                return "Energy Usage Normal";
-            }
-        }
+            return base.GetAlertLevel();  
     }
 }
--- SmartFactoryBackend/Sensors/GasLeakSensor.cs
+++ SmartFactoryBackend/Sensors/GasLeakSensor.cs
@@ -1,49 +1,26 @@
-﻿using System;
-
-namespace SmartFactoryBackend.Sensors
+﻿namespace SmartFactoryBackend.Sensors
 {
     public class GasLeakSensor : Sensor
     {
         public double GasConcentration { get; set; }
 
-        private double _upperThreshold = 5.0; 
-        private double _lowerThreshold = 0.1;  
 
+        public GasLeakSensor(string sensorId, AlarmTemplate alarmTemplate) : base(sensorId, "Gas Leak Sensor", alarmTemplate) { }
 
-       
-        public string GasLeakAlert { get; private set; }
-
-        public GasLeakSensor(string sensorId) : base(sensorId, "Gas Leak Sensor")
+   
+        public void ReadData()
         {
-            GasConcentration = 0;
-            GasLeakAlert = "Normal"; 
+            GasConcentration = new Random().NextDouble() * 10;
+            Console.WriteLine($"{Name} - Gas Concentration: {GasConcentration:F2} ppm");
+
+          
+            Console.WriteLine($"Gas Leak Alert Level: {GetAlertLevel()}");
         }
 
         
-        public void ReadData()
+        public override string GetAlertLevel()
         {
-            GasConcentration = new Random().NextDouble() * 10;  
-            Console.WriteLine($"{Name} - Gas Concentration: {GasConcentration:F2} ppm");
-
-            GasLeakAlert = CheckGasLeakAlert();
-            Console.WriteLine($"Alert: {GasLeakAlert}");
-        }
-
-
-        private string CheckGasLeakAlert()
-        {
-            if (GasConcentration > _upperThreshold)
-            {
-                return "Alert: High Gas Concentration!";
-            }
-            else if (GasConcentration < _lowerThreshold)
-            {
-                return "Alert: Gas Concentration Too Low!";
-            }
-            else
-            {
-                return "Gas Concentration Normal";
-            }
+            return base.GetAlertLevel(); 
         }
     }
 }
