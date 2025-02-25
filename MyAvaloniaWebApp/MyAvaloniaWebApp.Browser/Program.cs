84bda4c0c338624709038228d2e51e5cb13b4462
Anesa <akrslak17@gmail.com>
2025-02-25 16:14:38 +0100

    Create new branch for frontend development.

--- MyAvaloniaWebApp/MyAvaloniaWebApp.Browser/Program.cs
+++ MyAvaloniaWebApp/MyAvaloniaWebApp.Browser/Program.cs
@@ -10,6 +10,7 @@ internal sealed partial class Program
             .WithInterFont()
             .StartBrowserAppAsync("out");
 
+
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
 }
