e908be501bac7d5a34466b58795272e0cf035340
Anesa <akrslak17@gmail.com>
2025-02-25 16:12:58 +0100

    Add Avalonia for Frontend.

---
+++ MyAvaloniaWebApp/MyAvaloniaWebApp/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyAvaloniaWebApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
