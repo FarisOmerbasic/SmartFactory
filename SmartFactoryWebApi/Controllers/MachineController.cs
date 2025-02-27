c4e49a4f0d3b6d94bb4558f3cffa625bedca7370
Emin Branković <emin.brankovic19@gmail.com>
2025-02-27 08:43:48 +0100

    Added machine overview mock

---
+++ SmartFactoryWebApi/Controllers/MachineController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartFactoryWebApi.Dtos;
+using SmartFactoryWebApi.InMemoryRepositories;
+
+namespace SmartFactoryWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        [HttpGet("machineOverview")]
+        public ActionResult<MachineOverviewDto> MachineOverview()
+        {
+            var machineOverview = new MachineOverviewDto
+            {
+                RunningMachines = 30,
+                WarningMachinesThreshold = 3,
+                CriticalMachinesThreshold = 1,
+                IdleMachines = 3,
+                Machines = MachineRepository.Machines
+            };
+
+            return machineOverview;
+        }
+    }
+}
